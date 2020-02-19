cbb3d62caba085cbf88ad689bd7aa4a0ffda9b44
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-19 21:09:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,10 +15,57 @@ WIN_COMBINATIONS = [
   [6,4,2]
   ]
   
-  def won?(board)
-for each.WIN_COMBINATIONS do |number|
   
+def won?(board)
+  WIN_COMBINATIONS.each {|win_list|
+    index_0 = win_list[0]
+    index_1 = win_list[1]
+    index_2 = win_list[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_list
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_list
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
   else
-    false
+    return false
   end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
 end
